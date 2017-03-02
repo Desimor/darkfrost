22d5c0d19e18d37b14aa552f1595346cdf41b4a4
Matthew Morais <moraismatthew@yahoo.com>
2017-03-01 20:23:11 -0500

    Added the ability to have users input latitude and lonitude then click an update button that requests new weather data using that latitude and lonitude. The widget updates when the request completes.

--- public/js/app.js
+++ public/js/app.js
@@ -14,11 +14,29 @@ var currentlyWidget = new Vue({
         apparentTemperature: 67.3,
         precipProbability: 0.30,
         humidity: 0.61,
-        location: 'Gainesville, Fl'
+        location: 'Gainesville, Fl',
+        latitude: 29.1,
+        longitude: -81.4
     },
     methods: {
         iconUrl: function(iconString){
             return `/images/${iconString}.png`;
+        },
+        updateWeather: function(){
+            var url = `/weather/${this.latitude},${this.longitude}`;
+             axios.get(url)
+                .then(function(response){
+                var data = response.data.currently;
+                currentlyWidget.time = data.time;
+                currentlyWidget.summary = data.summary;
+                currentlyWidget.icon = data.icon;
+                currentlyWidget.apparentTemperature = data.apparentTemperature;
+                currentlyWidget.precipProbability = data.precipProbability;
+                currentlyWidget.humidity = data.humidity;
+            })
+            .catch(function(err){
+                console.log(err);
+            });
         }
     },
     created: function(){
