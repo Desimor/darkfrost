a232aefe6239d1b263e186582856bf9ae285b930
Matthew Morais <moraismatthew@yahoo.com>
2017-03-02 19:52:14 -0500

    Created the hourly widget using VueJS. In the process, I learned how to manipulate JavaScript Date objects, handle arrays of data for rendering using the v-for directive, and realized that the this keyword in the promise .then handler needed to be changed to refer to the Vue instance so we bound the outer this context to the .then function.

--- public/js/app.js
+++ public/js/app.js
@@ -25,8 +25,8 @@ var currentlyWidget = new Vue({
         getWeather: function(lat, lon){
             var url = `/weather/${lat},${lon}`;
              axios.get(url)
-                .then(function(response){
-                var data = response.data.currently;
+                .then(function(res){
+                var data = res.data.currently;
                 currentlyWidget.time = data.time;
                 currentlyWidget.summary = data.summary;
                 currentlyWidget.icon = data.icon;
@@ -60,13 +60,55 @@ var dailyWidget = new Vue({
     },
     created: function(){
         axios.get('/weather/29.1,-81.4')
-            .then(function(response){
-                var data = response.data.daily;
+            .then(function(res){
+                var data = res.data.daily;
                 dailyWidget.summary = data.summary;
                 dailyWidget.icon = data.icon;
             })
             .catch(function(err){
                 console.log(err);
             });
+    }
+});
+
+var hourlyWidget = new Vue({
+    el: "#hourly",
+    data: {
+        summary: "It's gonna rain!",
+        icon: 'clear-night',
+        hours: []
+    },
+    methods: {
+        getHourlyIcon: function(iconString){
+            return `/images/${iconString}.png`;
+        },
+        getDate: function(seconds){
+            var date = new Date(seconds * 1000);
+            var month = date.getMonth();
+            var year = date.getFullYear();
+            var day = date.getDate();
+            var hours = date.getHours();
+            var minutes = date.getMinutes();
+            return `${month}/${day}/${year} ${hour}"${minutes}`
+        },
+        getMainIcon: function(){
+            return `/images/${this.icon}.png`;
+        },
+        getHourlyWeather: function(lat, lon){
+            var url = `/weather/${lat},${lon}`;
+            axios.get(url)
+                .then(function(res){
+                    var hourlyData = res.data.hourly;
+                    this.summary = hourlyData.summary;
+                    this.icon = hourlyData.icon;
+                    this.hours = hourlyData.data;
+                })
+                .catch(function(err){
+                    console.log(err);
+                }.bind(this));
+        }
+    },
+    created: function(){
+        this.getHourlyWeather(29.1, -81.4);
     }
 });
